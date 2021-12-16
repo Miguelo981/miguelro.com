054dd533567702cd4f386276998ff27a3de57ace
Miguelo981 <lorito20@hotmail.es>
2021-12-16 16:07:43 +0100

    - G ID updated

--- src/config/google/google-analytics.config.ts
+++ src/config/google/google-analytics.config.ts
@@ -1,8 +1,8 @@
 const googleManagerScript = document.createElement('script');
 googleManagerScript.async = true;
-googleManagerScript.src = "https://www.googletagmanager.com/gtag/js?id=UA-135254749-4";
+googleManagerScript.src = "https://www.googletagmanager.com/gtag/js?id=G-TEVMF4DK2C";
 
 const googleConfigScript = document.createElement('script');
-googleConfigScript.innerHTML = "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-135254749-4');";
+googleConfigScript.innerHTML = "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-TEVMF4DK2C');";
   
 export const googleAnalytiscscripts = [googleManagerScript, googleConfigScript];
