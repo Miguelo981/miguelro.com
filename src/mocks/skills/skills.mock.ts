0af91b600f0efa005e0a31bc2dfbe5a1e1486618
Miguelo981 <lorito20@hotmail.es>
2021-12-20 14:20:21 +0100

    - Webp support added
    - Lazyload to images

--- src/mocks/skills/skills.mock.ts
+++ src/mocks/skills/skills.mock.ts
@@ -3,32 +3,32 @@ import { Skill, SkillList } from "src/app/models/skill.model"
 export const programmingLanguages: Skill[] = [
     {
         name: 'Javascript',
-        icon: "/assets/img/icons/js.png",
+        icon: "/assets/img/icons/js.webp",
         delay: 250
     },
     {
         name: 'Typescript',
-        icon: "/assets/img/icons/ts.png",
+        icon: "/assets/img/icons/ts.webp",
         delay: 500
     },
     {
         name: 'Python',
-        icon: "/assets/img/icons/py.png",
+        icon: "/assets/img/icons/py.webp",
         delay: 750
     },
     {
         name: 'Java',
-        icon: "/assets/img/icons/java.png",
+        icon: "/assets/img/icons/java.webp",
         delay: 1000
     },
     {
         name: 'Go',
-        icon: "/assets/img/icons/go.png",
+        icon: "/assets/img/icons/go.webp",
         delay: 1250
     },
     {
         name: 'PHP',
-        icon: "/assets/img/icons/php.png",
+        icon: "/assets/img/icons/php.webp",
         delay: 1500
     },
 
@@ -37,42 +37,42 @@ export const programmingLanguages: Skill[] = [
 export const frameworks: Skill[] = [
     {
         name: "Docker",
-        icon: "/assets/img/icons/docker.png",
+        icon: "/assets/img/icons/docker.webp",
         delay: 1750
     },
     {
         name: "VueJs",
-        icon: "/assets/img/icons/vuejs.png",
+        icon: "/assets/img/icons/vuejs.webp",
         delay: 2000
     },
     {
         name: "Angular",
-        icon: "/assets/img/icons/angular.png",
+        icon: "/assets/img/icons/angular.webp",
         delay: 2250
     },
     {
         name: "Django",
-        icon: "/assets/img/icons/django.png",
+        icon: "/assets/img/icons/django.webp",
         delay: 2500
     },
     {
         name: "Laravel",
-        icon: "/assets/img/icons/laravel.png",
+        icon: "/assets/img/icons/laravel.webp",
         delay: 2750
     },
     {
         name: "TailwindCss",
-        icon: "/assets/img/icons/tailwindcss.png",
+        icon: "/assets/img/icons/tailwindcss.webp",
         delay: 3000
     },
     {
         name: "Bootstrap",
-        icon: "/assets/img/icons/bootstrap.png",
+        icon: "/assets/img/icons/bootstrap.webp",
         delay: 3250
     },
     {
         name: "Git",
-        icon: "/assets/img/icons/git.png",
+        icon: "/assets/img/icons/git.webp",
         delay: 3500
     },
 ]
@@ -80,32 +80,32 @@ export const frameworks: Skill[] = [
 export const programs: Skill[] = [
     {
         name: "Visual Studio",
-        icon: "/assets/img/icons/vs.png",
+        icon: "/assets/img/icons/vs.webp",
         delay: 3750
     },
     {
         name: "Jetbrains IDEs",
-        icon: "/assets/img/icons/jetbrains.png",
+        icon: "/assets/img/icons/jetbrains.webp",
         delay: 4000
     },
     {
         name: "Postman",
-        icon: "/assets/img/icons/postman.png",
+        icon: "/assets/img/icons/postman.webp",
         delay: 4250
     },
     {
         name: "Wordpress",
-        icon: "/assets/img/icons/wordpress.png",
+        icon: "/assets/img/icons/wordpress.webp",
         delay: 4500
     },
     {
         name: "Elementor",
-        icon: "/assets/img/icons/elementor.png",
+        icon: "/assets/img/icons/elementor.webp",
         delay: 4750
     },
     {
         name: "AdobeXd",
-        icon: "/assets/img/icons/adobexd.png",
+        icon: "/assets/img/icons/adobexd.webp",
         delay: 5000
     }
 ]
@@ -113,17 +113,17 @@ export const programs: Skill[] = [
 export const dbs: Skill[] = [
     {
         name: "Mongodb",
-        icon: "/assets/img/icons/mongodb.png",
+        icon: "/assets/img/icons/mongodb.webp",
         delay: 5250
     },
     {
         name: "Postgre",
-        icon: "/assets/img/icons/postgre.png",
+        icon: "/assets/img/icons/postgre.webp",
         delay: 5500
     },
     {
         name: "MySQL",
-        icon: "/assets/img/icons/sql.png",
+        icon: "/assets/img/icons/sql.webp",
         delay: 5750
     },
 ]
