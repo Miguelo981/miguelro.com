63721b0055aa1c4cbfa079f6d0059ce33a7f4dee
Miguelo981 <lorito20@hotmail.es>
2022-01-06 17:15:43 +0100

    env updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendHost: "http://212.227.149.142:4000",
+  backendHost: "https://api.miguelo.dev",//"http://212.227.149.142:4000",
   backendVersion: "/v1",
   apiKey: 'test'
 };
